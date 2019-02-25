eaedd62154c8aff1b606f9a867cee979d65b8a09
Teddy Heinen <skyshayde@gmail.com>
2019-02-24 20:00:21 -0600

    most of the data structures

--- Pizza.cpp
+++ Pizza.cpp
@@ -1,5 +1,24 @@
-//
-// Created by skysh on 2/24/2019.
-//
-
+#include <stdexcept>
 #include "Pizza.h"
+
+void Pizza::createSlice(Slice slice) {
+    didMakeSlice = true;
+    slices.push_back(slice);
+    for(int i = slice.topLeft.x; i < slice.bottomRight.x;i++) {
+        for(int j = slice.topLeft.y; j < slice.bottomRight.y;j++) {
+            if(cells.at(i).at(j).isPartOfSlice == true) throw runtime_error("slice overlaps another slice");
+            cells.at(i).at(j).isPartOfSlice = true;
+            cells.at(i).at(j).slice = &slice;
+        }
+    }
+}
+
+void Pizza::replaceSlice(int index) {
+
+}
+
+int Pizza::sum() {
+    return 0;
+}
+
+Pizza::Pizza(int x, int y): x(x), y(y) {}
--- Pizza.h
+++ Pizza.h
@@ -1,14 +1,26 @@
-//
-// Created by skysh on 2/24/2019.
-//
-
 #ifndef PIZZA_HASHCODE_PIZZA_H
 #define PIZZA_HASHCODE_PIZZA_H
 
 
+#include "Slice.h"
+#include "Cell.h"
+#include "Pos.h"
+#include <vector>
+
+using namespace std;
+
 class Pizza {
+public:
+    vector<vector<Cell>> cells;
+    vector<Slice> slices;
+    bool didMakeSlice;
+    int x;
+    int y;
+    Pizza(int x, int y);
 
-
+    void createSlice(Slice slice);
+    void replaceSlice(int index);
+    int sum();
 
 };
 
