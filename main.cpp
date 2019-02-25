41fae93bb4350e90122c5b105636167afb6848cf
ShreyShah33 <shreyshah33@tamu.edu>
2019-02-24 20:24:16 -0600

    added main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Pizza.h"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+Pizza input_get(const string& name) {
+    
+
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        cout << "g++ main <input_file_name> <output_file_name>\n";
+    }
+    else{
+        string input ="a_example.in";
+        ifstream in (input);
+        if(in.is_open()){
+            int rows, cols, min, max;
+            in >> rows >> cols >> min >> max;
+            cout<<rows<<" "<<cols<<" "<<min<<" "<<max;
+            Pizza pizza(rows,cols);
+            for(int i=0; i<rows; i++){
+                for(int j=0; j<cols; j++){
+                    char ingredient;
+                    in>>ingredient;
+                    if(ingredient=='M') pizza.cells[i][j].isMushroom=true;
+                    else pizza.cells[i][j].isMushroom=false;
+                }
+            }
+        }        
+    }
+
 }
